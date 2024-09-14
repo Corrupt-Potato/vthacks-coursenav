76f6bccd5adefe7b8536426caad96ff28154488e
TJPNick <TJPNick@gmail.com>
2024-09-14 01:44:39 -0400

    added slight styling

--- script.js
+++ script.js
@@ -1,29 +1,63 @@
 let canvas;
 let ctx;
+let selectedCourse;
 let courses = [];
-window.onload = function(){
+window.onload = function () {
     //alert('hi')
     //make request to data
     canvas = document.getElementById('courseCanvas');
     ctx = canvas.getContext('2d')
-    interval = setInterval(render,25);
+    interval = setInterval(render, 25);
     loadCourses()
+    startTime = Date.now()
 }
-async function loadCourses(){
+async function loadCourses() {
     let data = await (fetch("data.json").then(x => x.json()))
     courses = data["courses"]
     let dataElement = document.getElementById("courses")
-    for(let i = 0; i<courses.length; i++){
+    for (let i = 0; i < courses.length; i++) {
         let newData = document.createElement("option")
         newData.value = courses[i].id
         newData.innerText = courses[i].name
         dataElement.appendChild(newData)
     }
 }
-function render(){
+let startTime = 0;
+function render() {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
-    ctx.fillStyle = "#ff0000"
-    ctx.fillRect(0,0,canvas.width,canvas.height)
+    ctx.fillStyle = "#861F41"
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
+    ctx.strokeStyle = "#E5751F"
+    ctx.lineWidth = 10;
+    let runTime = Date.now() - startTime
+    for (let i = (runTime / 50) % 100 - 50; i < canvas.height / 5 + canvas.width; i += 50) {
+        ctx.beginPath();
+        ctx.moveTo(i - canvas.height / 5, 0);
+        ctx.lineTo(i, canvas.height);
+        ctx.stroke()
+    }
+}
+
+function navigate() {
+    selectedCourse = null;
+    let targetCourseID = document.getElementById("selectedCourse").value;
+    for (let i = 0; i < courses.length; i++) {
+        if(courses[i].id == targetCourseID){
+            selectedCourse = courses[i]
+        }
+    }
+    if(selectedCourse == null){
+        //invalid entry
+        return;
+    }
+    document.getElementById("overlay").hidden = true;
+    document.getElementById("exitButton").hidden = false;
+}
+function exitNav(){
+    selectedCourse = null;
+    document.getElementById("selectedCourse").value = "";
+    document.getElementById("overlay").hidden = false;
+    document.getElementById("exitButton").hidden = true;
 }
 let interval;
